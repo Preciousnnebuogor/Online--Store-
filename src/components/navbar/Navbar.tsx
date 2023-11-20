c365caa1c473fd63f661559e8a5ca39e0bbc79e2
Preciousnnebuogor <osemekepreciousnnebuogor@gmail.com>
2023-11-20 18:30:37 +0000

    first commit

---
+++ src/components/navbar/Navbar.tsx
@@ -0,0 +1,20 @@
+import Link from "next/link"
+import style from "./Nav.module.scss"
+export default function Navbar() :JSX.Element {
+    return (
+         <nav className={style.nav}>
+            <h1 className={style.logo}>Perfection</h1>
+
+            <ul className={style.link}>
+                <Link href="">Home</Link>
+                <Link href="">Categories</Link>
+                <Link href="">Contact</Link>
+                <Link href="">Help</Link>
+
+            </ul>
+
+
+         </nav>
+    ) 
+    
+} 
