8608131a9c182780ccb776a050a6ea3cef611ad4
Preciousnnebuogor <osemekepreciousnnebuogor@gmail.com>
2023-11-21 15:41:38 +0000

    completed hero section

---
+++ src/components/hero/Hero.tsx
@@ -0,0 +1,21 @@
+import Image from "next/image";
+import style from "./Hero.module.scss";
+export default function Hero() {
+  return (
+    <div className={style.content}>
+      <div className={style.word}>
+        <h1>Fashion Men</h1>
+        <p>
+          Perfection online store gives you the privilege to shop online from
+          your comfort zone. We also ensure all clothes display are of high
+          quality, are pocket friendly and delivery to all allocations in
+          Nigeria. <br/> Classy and confident, the epitome of fine taste.
+        </p>
+        <button>Click</button>
+      </div>
+      <div className={style.photo}>
+        <img src="/ph.png"></img>
+      </div>
+    </div>
+  );
+}
