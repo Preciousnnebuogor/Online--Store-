46ac8fa5434c5b6105a7bfc4e048521d81e290c9
Preciousnnebuogor <osemekepreciousnnebuogor@gmail.com>
2023-11-25 21:38:39 +0000

    pages file added

---
+++ src/pages/Categories.tsx
@@ -0,0 +1,5 @@
+export default function Cateegories (){
+    return <div>
+        <h1>Categories</h1>
+    </div>
+}
---
+++ src/pages/Help.tsx
@@ -0,0 +1,5 @@
+export default function Help () {
+    return <div>
+        <h2>Help</h2>
+    </div>
+}
