ea3cf15fa4f7030f7a6464131273ca103619155f
Preciousnnebuogor <osemekepreciousnnebuogor@gmail.com>
2023-11-26 19:40:13 +0000

    Contact page

--- src/pages/contact.tsx
+++ src/pages/contact.tsx
@@ -1,5 +1,23 @@
 export default function Contact () {
-    return <div>
-     <div>Contact for more enquires</div>
-    </div>
+    return <form>
+     <div>
+        <h2>Contact Page</h2>
+        <p>for more information or complain contact us below</p>
+        </div>
+        <label>Firstname</label>
+        <input />
+
+        <label>Middlename</label>
+        <input />
+
+        <label>Surname</label>
+        <input />
+
+        <button>Submit</button>
+
+        
+
+
+     </form>
+    
 }
