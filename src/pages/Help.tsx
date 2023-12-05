4312d93c71525e8f32c787bb1e906c76695cc136
Preciousnnebuogor <osemekepreciousnnebuogor@gmail.com>
2023-12-05 21:11:17 +0000

    added productdetails page

--- src/pages/Help.tsx
+++
@@ -1,5 +0,0 @@
-export default function Help () {
-    return <div>
-        <h2>Help</h2>
-    </div>
-}
---
+++ src/pages/products/[slug]/index.tsx
@@ -0,0 +1,3 @@
+export default function ProductDetailsPage() {
+    return <div>product details page</div>
+}
