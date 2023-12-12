b38c5c73fba26cf338a881dc32450b00a3baa329
Preciousnnebuogor <osemekepreciousnnebuogor@gmail.com>
2023-12-12 22:01:12 +0000

    changes

--- src/components/toturial/html.tsx
+++ src/components/toturial/html.tsx
@@ -1,15 +1,16 @@
- import style from "./css.module.css"
- export default function Html () {
-   return <div className={style.body}>
-    <h1>i am a girl</h1> 
-       <h2>i love you</h2>
-//        <div className={style.block}>we are okay</div>
-//        <div className={style.block2}>we are fine</div>
-//        <div className={style.block3}>we are better</div>
-//        <h2>beginning of love</h2>
-//        <span className={style.inline}>inline good</span>
-//        <span className={style.inline2}> inline good</span>
-//        <span className={style.inline3}> inline good</span>
-
-   </div>
- }
+import style from "./css.module.css";
+export default function Html() {
+  return (
+    <div className={style.body}>
+      <h1>i am a girl</h1>
+      <h2>i love you</h2>
+      <div className={style.block}>we are okay</div>
+      <div className={style.block}>we are fine</div>
+      <div className={style.block}>we are better</div>
+      <h2>beginning of love</h2>
+      <span className={style.inline}>inline good</span>
+      <span className={style.inline}> inline good</span>
+      <span className={style.inline}> inline good</span>
+    </div>
+  );
+}
