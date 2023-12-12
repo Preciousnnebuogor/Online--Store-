819e399044cf743a8d8645347c58df50b075dc1e
Preciousnnebuogor <osemekepreciousnnebuogor@gmail.com>
2023-12-12 21:35:53 +0000

    tsx and css

--- src/components/toturial/html.tsx
+++ src/components/toturial/html.tsx
@@ -1,9 +1,15 @@
-import style from "./css.module.css"
-export default function Html () {
-    return <div>
-       <h1>i am a girl</h1> 
+ import style from "./css.module.css"
+ export default function Html () {
+   return <div className={style.body}>
+    <h1>i am a girl</h1> 
        <h2>i love you</h2>
-       <div className={style.block}>we are okay</div>
-       <div className={style.block}>we are fine</div>
-    </div>
-}
+//        <div className={style.block}>we are okay</div>
+//        <div className={style.block}>we are fine</div>
+//        <div className={style.block}>we are better</div>
+//        <h2>beginning of love</h2>
+//        <span className={style.inline}>inline good</span>
+//        <span className={style.inline}> inline good</span>
+//        <span className={style.inline}> inline good</span>
+
+   </div>
+ }
