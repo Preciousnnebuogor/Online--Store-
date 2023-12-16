082d06360450839218b81f4fc1001d753f456368
Preciousnnebuogor <osemekepreciousnnebuogor@gmail.com>
2023-12-16 21:08:08 +0000

    copy css

--- src/components/toturial/html.tsx
+++ src/components/toturial/html.tsx
@@ -4,6 +4,7 @@ export default function Html() {
   return (
     <div className={style.body}>
       <h1>i am a girl</h1>
+      <hr/>
       <h2>i love you</h2>
       <div className={style.block}>we are okay</div>
       <div className={style.block}>we are fine</div>
