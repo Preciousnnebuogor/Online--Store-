65a61ffef8b979d3308e1737a185042f0987b992
Preciousnnebuogor <osemekepreciousnnebuogor@gmail.com>
2023-12-17 21:14:54 +0000

    new product

--- src/components/toturial/html.tsx
+++ src/components/toturial/html.tsx
@@ -1,45 +1,8 @@
-import Link from "next/link";
-import style from "./css.module.css";
-export default function Html() {
-  return (
-    <div className={style.body}>
-      <h1>i am a girl</h1>
-      <hr />
-      <h2>i love you</h2>
-      <div className={style.block}>we are okay</div>
-      <div className={style.block}>we are fine</div>
-      <div className={style.block}>we are better</div>
-      <h2>beginning of love</h2>
-      <span className={style.inline}>
-        inline<i>good cat</i> good
-      </span>
-      <span className={style.inline}> inline good</span>
-      <span className={style.inline}> inline good</span>
-      <div className={style.list}>
-        <ul className={style.copy}>
-          <Link href={""}>
-            <li>felix</li>
-          </Link>
-          <Link href={""}>
-            <li>patrick</li>
-          </Link>
-          <Link href={""}>
-            <li>kelvin</li>
-          </Link>
-          <Link href={""}>
-            <li>fidelis</li>
-          </Link>
-          <Link href={""}>
-            <li>olise</li>
-          </Link>
+import Link from "next/link"
+export default function HTML(){
+    return <div>
+        <ul>
+           <li><Link href={""}>Access</Link></li> 
         </ul>
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Obcaecati
-          ipsam nemo rerum excepturi eligendi, cupiditate magnam molestias
-          ipsum, laborum distinctio numquam odit facere nam eos enim impedit
-          magni velit cum!
-        </p>
-      </div>
     </div>
-  );
 }
