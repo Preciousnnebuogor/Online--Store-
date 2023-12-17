c190cff50c92494dbda8638d24632dff971b7af7
Preciousnnebuogor <osemekepreciousnnebuogor@gmail.com>
2023-12-17 21:16:56 +0000

    new product

--- src/components/toturial/html.tsx
+++ src/components/toturial/html.tsx
@@ -2,7 +2,10 @@ import Link from "next/link"
 export default function HTML(){
     return <div>
         <ul>
-           <li><Link href={""}>Access</Link></li> 
+           <li><Link href={""}>Access</Link></li>
+           <li><Link href={""}>Project</Link></li>
+           <li><Link href={""}>Product</Link></li>
+           <li><Link href={""}>Register</Link></li> 
         </ul>
     </div>
 }
