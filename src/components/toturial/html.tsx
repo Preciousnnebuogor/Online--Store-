e7b71969c74e5cbd2d95d4f2138ad723b0d10d14
Preciousnnebuogor <osemekepreciousnnebuogor@gmail.com>
2023-12-17 22:45:56 +0000

    new product

--- src/components/toturial/html.tsx
+++ src/components/toturial/html.tsx
@@ -2,23 +2,37 @@ import Link from "next/link";
 import style from "./css.module.scss";
 export default function HTML() {
   return (
+    <>
     <div className={style.link}>
-      <ul>
-        <li>
-          <Link href={""}>Access</Link>
-        </li>
-        <li>
-          <Link href={""}>Project</Link>
-        </li>
-        <li>
-          <Link href={""}>Product</Link>
-        </li>
-        <li>
-          <Link href={""}>Register</Link>
-        </li>
-      </ul>
+          <ul>
+              <li>
+                  <Link href={""}>Access</Link>
+              </li>
+              <li>
+                  <Link href={""}>Project</Link>
+              </li>
+              <li>
+                  <Link href={""}>Product</Link>
+              </li>
+              <li>
+                  <Link href={""}>Register</Link>
+              </li>
+          </ul>
+          <button>Click</button>
+      </div>
+      
+      <div className={style.page}>
+              <li><Link href={""}>Home</Link></li>
+              <li><Link href={""}>Page</Link></li>
+              <li><Link href={""}>About</Link></li>
+              <li><Link href={""}>Log Out</Link></li>
 
-      <button>Click</button>
-    </div>
+
+
+          </div>
+          
+          </>
   );
+  
+   
 }
