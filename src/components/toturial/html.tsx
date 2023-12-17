d6bbbd409b23515ade9cea849e82f0c563a8dd4d
Preciousnnebuogor <osemekepreciousnnebuogor@gmail.com>
2023-12-17 21:53:27 +0000

    new product

--- src/components/toturial/html.tsx
+++ src/components/toturial/html.tsx
@@ -1,12 +1,22 @@
-import Link from "next/link"
-import style from "./css.module.css"
-export default function HTML(){
-    return <div className={style.link}>
-        <ul>
-           <li><Link href={""}>Access</Link></li>
-           <li><Link href={""}>Project</Link></li>
-           <li><Link href={""}>Product</Link></li>
-           <li><Link href={""}>Register</Link></li> 
-        </ul>
+import Link from "next/link";
+import style from "./css.module.scss";
+export default function HTML() {
+  return (
+    <div className={style.link}>
+      <ul>
+        <li>
+          <Link href={""}>Access</Link>
+        </li>
+        <li>
+          <Link href={""}>Project</Link>
+        </li>
+        <li>
+          <Link href={""}>Product</Link>
+        </li>
+        <li>
+          <Link href={""}>Register</Link>
+        </li>
+      </ul>
     </div>
+  );
 }
