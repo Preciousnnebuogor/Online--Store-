0011c28776ea9fe9290f80b5b6c35fc3ee3f6e23
Preciousnnebuogor <osemekepreciousnnebuogor@gmail.com>
2023-12-19 19:03:13 +0000

    css for img

--- src/components/click/ClickMe.tsx
+++ src/components/click/ClickMe.tsx
@@ -56,13 +56,20 @@ export default function ClickMe() {
       </div>
       <div className={style.img}>
         <img src="/cc1.png" />
-      
+      <div className={style.para}>
       <p>
         We have made fashioning for our classic men more easy,reliable and cost
         effective making your shopping online fast and reliable. As a result
         payment can be made on delivery to any location of your choice.
+        Perfection your NO 1 Shopping App.  We have made fashioning for our classic men more easy,reliable and cost
+        effective making your shopping online fast and reliable. As a result
+        payment can be made on delivery to any location of your choice.
+        Perfection your NO 1 Shopping App.  We have made fashioning for our classic men more easy,reliable and cost
+        effective making your shopping online fast and reliable. As a result
+        payment can be made on delivery to any location of your choice.
         Perfection your NO 1 Shopping App
       </p>
+      </div>
       </div>
     </>
   );
