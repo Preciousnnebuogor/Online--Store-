71bf671f7708b96cee4e41f8739420184d320f55
Preciousnnebuogor <osemekepreciousnnebuogor@gmail.com>
2024-01-04 23:26:50 +0000

    tutorial page removed

--- src/components/404/404.tsx
+++ src/components/404/404.tsx
@@ -7,10 +7,10 @@ export default function NoteFound() {
       <h1 className={style.text}>Ooooops...</h1>
       <h2 className={style.text2}>That page cannot be found.</h2>
       <p className={style.para}>
-        Go back to the<Link href="/">Homepage</Link>
+        Go back to the   <Link href="/">Homepage</Link>
       </p>
 
-      <h2 className={style.copy}>copyright 2023 Online-Store</h2>
+      <h2 className={style.copy}>copyright 2024 Online-Store</h2>
     </div>
   );
 }
--- src/pages/tutorial.tsx
+++
@@ -1,8 +0,0 @@
-import Html from "@/components/toturial/html";
-
-export default function () {
-  return <div>
-  <Html/>
-    
-  </div>;
-}
